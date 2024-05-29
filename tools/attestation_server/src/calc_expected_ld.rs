8969bf684f41bb9b917f9fe7da86d3a5c9086594
Gianluca Scopelliti <gianluca.scopelliti@ericsson.com>
2024-05-29 09:29:54 +0000

    attestation-server: adapt to new `sev` version

--- tools/attestation_server/src/calc_expected_ld.rs
+++ tools/attestation_server/src/calc_expected_ld.rs
@@ -5,6 +5,7 @@ use sev::measurement::idblock_types::ID_BLK_ID_BYTES;
 use sev::measurement::{
     snp::{snp_calc_launch_digest, SnpMeasurementArgs},
     vmsa::{GuestFeatures, VMMType},
+    vcpu_types::CpuType
 };
 use snafu::{ResultExt, Whatever};
 
@@ -41,7 +42,7 @@ impl VMDescription {
     pub fn compute_expected_hash(&self) -> Result<[u8; 384 / 8], Whatever> {
         let snp_measure_args = SnpMeasurementArgs {
             vcpus: self.vcpu_count,
-            vcpu_type: "EPYC-v4".into(),
+            vcpu_type: CpuType::EpycV4,
             ovmf_file: self.ovmf_file.clone().into(),
             guest_features: self.guest_features,
             kernel_file: Some(self.kernel_file.clone().into()),
